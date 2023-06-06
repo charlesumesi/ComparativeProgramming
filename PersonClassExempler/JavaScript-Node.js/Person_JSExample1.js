5f0baa6b0f11b140dda5218cc0d70e53bf5989ec
charlesumesi <54457600+charlesumesi@users.noreply.github.com>
2023-06-06 15:51:06 +0100

    Add files via upload

--- PersonClassExempler/JavaScript-Node.js/Person_JSExample1.js
+++ PersonClassExempler/JavaScript-Node.js/Person_JSExample1.js
@@ -1,1 +1,23 @@
+// Parent class
+class Person {
+  constructor(first, last, age, gender, interests) {
+    this.first = first
+    this.last = last;
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+  }
 
+  just_firstandlast() {
+    return this.first + ' ' + this.last;
+  };
+
+  full() {
+    this.a = 'is a';
+    this.b = 'year old';
+    this.c = 'interested in';
+    console.log(`${this.first} ${this.last} ${this.a} ${this.age} ${this.b} ${this.gender} ${this.c} ${this.interests}`);
+  };
+}
+
+module.exports = { Person }
