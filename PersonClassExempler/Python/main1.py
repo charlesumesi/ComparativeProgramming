ad30b4f29c608921c5d43fdcd14eb185ebddee7e
charlesumesi <54457600+charlesumesi@users.noreply.github.com>
2023-06-06 15:47:05 +0100

    Add files via upload

---
+++ PersonClassExempler/Python/main1.py
@@ -0,0 +1,7 @@
+import Person_PythonExample1 as person
+
+# Person instance (object)
+han = person.Person('Han', 'Solo', '30', 'man', 'flying spaceships')
+
+print(han.just_firstandlast())
+han.full()
