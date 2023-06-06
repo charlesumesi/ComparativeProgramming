ff7f1f1e3aed42e0e166efebce3c542620fabf93
charlesumesi <54457600+charlesumesi@users.noreply.github.com>
2023-06-06 15:54:23 +0100

    Add files via upload

--- PersonClassExempler/C++/Person_C++Example1.h
+++ PersonClassExempler/C++/Person_C++Example1.h
@@ -1,1 +1,8 @@
+#include <string>
 
+// Parent class
+class Person {
+  public:
+    void just_firstandlast(std::string first, std::string last);
+    void full(std::string first, std::string last, std::string age, std::string gender, std::string interests);
+};
