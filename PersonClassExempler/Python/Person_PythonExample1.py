ff154747fdab89e4305b0ee68126b61260a109da
charlesumesi <54457600+charlesumesi@users.noreply.github.com>
2023-06-06 15:41:06 +0100

    Add files via upload

--- PersonClassExempler/Python/Person_PythonExample1.py
+++ PersonClassExempler/Python/Person_PythonExample1.py
@@ -1,1 +1,17 @@
-
+# Parent class
+class Person:
+    def __init__(self, first, last, age, gender, interests):
+        self.first = first
+        self.last = last
+        self.age = age
+        self.gender = gender
+        self.interests = interests
+        
+    def just_firstandlast(self):
+        return(self.first + ' ' + self.last)
+    
+    def full(self):
+        a = 'is a'
+        b = 'year old'
+        c = 'interested in'
+        print(f'{Person.just_firstandlast(self)} {a} {self.age} {b} {self.gender}, {c} {self.interests}')
