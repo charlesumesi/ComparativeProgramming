c0beef33757500dc047bec4141e79af9f09bb976
charlesumesi <54457600+charlesumesi@users.noreply.github.com>
2023-06-07 21:18:24 +0100

    Add files via upload

--- PersonClassExempler/C++/main1.cpp
+++ PersonClassExempler/C++/main1.cpp
@@ -3,28 +3,6 @@
 #include "Person_C++Example1.h"
 using namespace std;
 
-void Person::just_firstandlast(string first, string last) {
-  cout << first + " " + last;
-}
-
-void Person::full(string first, string last, string age, string gender, string interests) {
-  string a = "is a";
-  string b = "year old";
-  string c = "interested in";
-  string space = " ";
-  string comma = ", ";
-
-  string array[8] = {first, last, a, age, b, gender, c, interests};
-
-  for (int i = 0; i < 8; i++) {
-    if (i != 5) {
-      cout << array[i] + space;
-    } else {
-      cout << array[i] + comma;
-    }
-  }
-}
-
 int main() {
   // Person instance (object)
   Person han;
