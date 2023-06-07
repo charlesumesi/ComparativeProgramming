1d9d80ae4c3bd69c6ae259994077fa41c37904b4
charlesumesi <54457600+charlesumesi@users.noreply.github.com>
2023-06-07 21:18:52 +0100

    Add files via upload

--- PersonClassExempler/C++/Person_C++Example1.h
+++ PersonClassExempler/C++/Person_C++Example1.h
@@ -1,4 +1,5 @@
 #include <string>
+#include <iostream>
 
 // Parent class
 class Person {
@@ -6,3 +7,25 @@ class Person {
     void just_firstandlast(std::string first, std::string last);
     void full(std::string first, std::string last, std::string age, std::string gender, std::string interests);
 };
+
+void Person::just_firstandlast(std::string first, std::string last) {
+  std::cout << first + " " + last;
+}
+
+void Person::full(std::string first, std::string last, std::string age, std::string gender, std::string interests) {
+  std::string a = "is a";
+  std::string b = "year old";
+  std::string c = "interested in";
+  std::string space = " ";
+  std::string comma = ", ";
+
+  std::string array[8] = {first, last, a, age, b, gender, c, interests};
+
+  for (int i = 0; i < 8; i++) {
+    if (i != 5) {
+      std::cout << array[i] + space;
+    } else {
+      std::cout << array[i] + comma;
+    }
+  }
+}
