862dff0df09edf72aba1908b01647021b5131956
charlesumesi <54457600+charlesumesi@users.noreply.github.com>
2023-06-08 20:13:09 +0100

    Add files via upload

---
+++ PersonClassExempler/Python/Person_PythonExample2.py
@@ -0,0 +1,5 @@
+import Person_PythonExample1 as person
+
+# New class inherits all methods in Person
+class Student(person.Person):
+    pass
---
+++ PersonClassExempler/Python/main2.py
@@ -0,0 +1,7 @@
+from Person_PythonExample2 import Student
+
+# Student instance (object)
+peter = Student('Peter', 'Davis', '15', 'teenager','football')
+
+print(peter.just_firstandlast())
+peter.full()
