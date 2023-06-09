7c68fe96e31bafff2513052380729830e2b788ae
charlesumesi <54457600+charlesumesi@users.noreply.github.com>
2023-06-09 19:27:48 +0100

    Update Person_JSExample2.js

--- PersonClassExempler/JavaScript-Node.js/Person_JSExample2.js
+++ PersonClassExempler/JavaScript-Node.js/Person_JSExample2.js
@@ -1,7 +1,7 @@
-const student = require("./Person_JSExample1");
+const person2 = require("./Person_JSExample1");
 
 // New class inherits all methods in Person
-class Student extends student.Person {
+class Student extends person2.Person {
   constructor(first, last, age, gender, interests) {
     super(first, last, age, gender, interests)
   }
